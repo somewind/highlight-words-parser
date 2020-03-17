291b5d7be006527bae28af44e6732017f4176333
maoshu <neverstopsteps@gmail.com>
2020-03-17 11:44:01 +0800

    feat: add babel compile

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        // 构建浏览器目标版本支持，将自动读取根目录 package.json 中的 browserslist
+        useBuiltIns: 'entry',
+        corejs: 3
+      }
+    ]
+  ]
+}
